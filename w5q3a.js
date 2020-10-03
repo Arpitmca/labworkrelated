731587a743d9b7a5da06426f173c1d5a83f75708
Arpit Kumar Gupta <70845226+Arpitmca@users.noreply.github.com>
2020-10-03 16:24:02 +0530

    Update w5q3a.js

--- w5q3a.js
+++ w5q3a.js
@@ -1,9 +1,9 @@
-var top="a1";
+var top1="a1";
 function toTop(newTop)
 {
-domTop = document.getElementById('top').style;
-domNew = document.getElementById('newTop').style;
+domTop = document.getElementById(top1).style;
+domNew = document.getElementById(newTop).style;
 domTop.zIndex="0";
 domNew.zIndex="10";
-top=newTop;
+top1=newTop;
 }
